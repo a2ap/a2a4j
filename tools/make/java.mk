6205ebb8bd51f3c9247c5eadd4c4a9911e226fed
shown.Ji <yuluo08290126@gmail.com>
2025-06-07 12:18:09 +0800

    fix
    Signed-off-by: shown.Ji <yuluo08290126@gmail.com>

--- tools/make/java.mk
+++ tools/make/java.mk
@@ -28,9 +28,4 @@ build: ## Build the project
 .PHONY: format-check
 format-check: ## Format Check the code
 	@$(LOG_TARGET)
-	mvn spring-javaformat:validate
-
-.PHONY: checkstyle-check
-checkstyle-check: ## Checkstyle Check the code and output to target/checkstyle-report.xml
-	@$(LOG_TARGET)
-	mvn clean compile -Dcheckstyle.skip=false -Dcheckstyle.output.file=checkstyle-report.xml checkstyle:checkstyle
+	mvn clean compile --file pom.xml
